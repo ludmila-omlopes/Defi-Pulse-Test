9312d37b560eeb916c6d2af494d5445ff11f7e01
ludmila-omlopes <ludmila.omlopes@gmail.com>
2021-05-04 14:34:00 -0300

    new changes

--- wp-login.php
+++ wp-login.php
@@ -92,7 +92,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="./wp-includes/js/wp-util.min.js?ver=5.7.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"5f67e439d8"};
+var userProfileL10n = {"user_id":"0","nonce":"c58ae80a72"};
 /* ]]> */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
