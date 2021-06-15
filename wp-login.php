545f268a4478b2d78d2a450791e95d029dc89330
ludmila-omlopes <ludmila.omlopes@gmail.com>
2021-06-15 15:57:58 -0300

    design fixes

--- wp-login.php
+++ wp-login.php
@@ -92,7 +92,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="./wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"8a6fa2a4b1"};
+var userProfileL10n = {"user_id":"0","nonce":"440e4350d3"};
 /* ]]> */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
